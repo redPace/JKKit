7af0448b21035fa1022197e43411392193d979fb
redPace <2455181821@qq.com>
2017-10-18 17:24:18 +0800

    上传文件
    上传文件

---
+++ JKKit.h
@@ -0,0 +1,21 @@
+//
+//  JKKit.h
+//  JKKit
+//
+//  Created by liqinghong123 on 2017/10/18.
+//  Copyright © 2017年 liqinghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Tool1.h"
+#import "Tool2.h"
+
+//! Project version number for JKKit.
+FOUNDATION_EXPORT double JKKitVersionNumber;
+
+//! Project version string for JKKit.
+FOUNDATION_EXPORT const unsigned char JKKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JKKit/PublicHeader.h>
+
+
